887c7f668b733e3027dc7bfa45d24ba71447453a
IrishEgie <irishegie.arao@ssu.edu.ph>
2024-10-13 10:47:54 +0800

    update: reading the csv file, but currently not being displayed to the html

--- main.py
+++ main.py
@@ -58,8 +58,8 @@ def cafes():
     with open('cafe-data.csv', newline='', encoding='utf-8') as csv_file:
         csv_data = csv.reader(csv_file, delimiter=',')
         list_of_rows = []
-        for row in csv_data:
-            list_of_rows.append(row)
+        list_of_rows = [row for row in csv_data]
+        print(list_of_rows) 
     return render_template('cafes.html', cafes=list_of_rows)
 
 
